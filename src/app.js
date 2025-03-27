31095017bfcfa2c4f43db65870b2d4703aadba25
PhilimonNag <70008047+PhilimonNag@users.noreply.github.com>
2025-03-28 04:53:14 +0530

    fix: update '/' route to return correct API response

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const errorHandler = require("./middlewares/errorHandler");
 
 dotenv.config(); // Load environment variables
 
@@ -9,10 +10,17 @@ const app = express();
 // Middleware
 app.use(express.json()); // Parse JSON requests
 app.use(cors()); // Enable CORS
+app.use(express.urlencoded({ extended: true }));
 
 // Test route
 app.get("/", (req, res) => {
-  res.send("API is running...");
+  res.send({ message: "API is running..." });
 });
 
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  res.status(404);
+  next(error);
+});
+app.use(errorHandler); // Error handling middleware
 module.exports = app;
