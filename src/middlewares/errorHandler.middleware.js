7b18b352e9fc9eb23c49b8c0362a404a8936227d
PhilimonNag <70008047+PhilimonNag@users.noreply.github.com>
2025-03-28 06:33:32 +0530

    fix:Custom message for duplicate error in error middleware

--- src/middlewares/errorHandler.middleware.js
+++ src/middlewares/errorHandler.middleware.js
@@ -1,12 +1,21 @@
 const responseHandler = require("../utils/responseHandler");
-
 const errorHandler = (err, req, res, next) => {
+  let statusCode = err.statusCode || 500;
+  let message = err.message || "Internal Server Error";
+
+  // Handle MongoDB Duplicate Key Error (E11000)
+  if (err.code === 11000) {
+    statusCode = 400;
+    const field = Object.keys(err.keyPattern)[0];
+    message = `The ${field} '${err.keyValue[field]}' is already taken. Please use a different ${field}.`;
+  }
+
   return responseHandler({
     res,
     success: false,
-    message: err.message || "Internal Server Error",
-    error: process.env.NODE_ENV === "development" ? err.stack : undefined,
-    statusCode: err.statusCode || 500,
+    message,
+    error: process.env.NODE_ENV === "development" ? err.stack : null,
+    statusCode,
   });
 };
 
