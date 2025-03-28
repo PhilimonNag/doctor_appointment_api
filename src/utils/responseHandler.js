49f88d5f14d1c0e80f6f9589c91361b03e1445e4
PhilimonNag <70008047+PhilimonNag@users.noreply.github.com>
2025-03-28 06:36:18 +0530

    fix: remove __v, createdAt, and updatedAt from response in responseHandler

--- src/utils/responseHandler.js
+++ src/utils/responseHandler.js
@@ -12,6 +12,14 @@ const responseHandler = ({
   error = null,
   statusCode = 200,
 }) => {
+  if (data && typeof data.toObject === "function") {
+    data = data.toObject();
+  }
+
+  if (data && typeof data === "object") {
+    const { createdAt, updatedAt, __v, ...filteredData } = data;
+    data = filteredData;
+  }
   const response = cleanResponse({ success, message, data, error });
   return res.status(statusCode).json(response);
 };
