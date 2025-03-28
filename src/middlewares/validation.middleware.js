c77b967ea0f33534cfe80d3cde4601660450f617
PhilimonNag <70008047+PhilimonNag@users.noreply.github.com>
2025-03-28 06:31:09 +0530

    fix: update validate middleware with single error

--- src/middlewares/validation.middleware.js
+++ src/middlewares/validation.middleware.js
@@ -3,7 +3,12 @@ const { validationResult } = require("express-validator");
 const validate = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ success: false, errors: errors.array() });
+    return res.status(400).json({
+      success: false,
+      message: "Validation error",
+      error: errors.array()[0]["msg"],
+      statusCode: 400,
+    });
   }
   next();
 };
